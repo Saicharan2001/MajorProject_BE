7650d4e73f9e5ea2b1142b068abefcb4b5b529d1
K.SAICHARAN <saicharan2001kande@gmail.com>
2023-03-11 18:30:19 +0530

    Mongoose

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require("express");
-const bodyparser = require("body-parser")
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyparser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// MONGOOSE CONNECTION
+mongoose.connect("mongodb+srv://saicharan2001kande:YKuaRNbkaCQ2QBGx@cluster1.0ptahnc.mongodb.net/?retryWrites=true&w=majority/Backend_MP").then(()=>{
+    console.log("Connection to mongoDB cloud is successful")
+}).catch((err) => console.log("Not connected"));
+
+const QA_Schema = new mongoose.Schema({
+    question : String,
+    answer : String,
+    date : Date
+});
+
+const QA_Model = mongoose.model("QandA",QA_Schema);
+
+const qa1 = new QA_Model({
+    question : "Cultivation and production of rice",
+    answer : "Preparation of field , transplantation of seeds , field maintenance , harvesting , Threshing , winnowing and milling"
+})
+
+
+
 
 app.get("/",function(req,res){
     res.send("<h1> Hello world </h1>");
-})
+});
 
-
-app.get("/contact",function(req,res){
-    res.send("This is SaiCharan")
-})
-app.listen(3000,function(){
-    console.log("Server started at port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
 });
